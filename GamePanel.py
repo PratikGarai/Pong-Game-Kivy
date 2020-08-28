5e06317b9843cdcc07fb6d283c1bf76d1460bf2c
PratikGarai <pratikgarai0208@gmail.com>
2020-08-29 02:31:08 +0530

    Added the scoreboard

--- GamePanel.py
+++ GamePanel.py
@@ -7,8 +7,10 @@ from kivy.clock import Clock
 class GamePanel(RelativeLayout):
     def __init__(self, **kwargs):
         super(GamePanel, self).__init__(**kwargs)
-        self.b1 = Label(text="Left")
-        self.add_widget(self.b1)
+        self.s1 = Label(text="0")
+        self.s2 = Label(text="0")
+        self.add_widget(self.s1)
+        self.add_widget(self.s2)
 
         self.PaddleRight = Paddle(5)
         self.PaddleLeft = Paddle(5)
@@ -26,7 +28,7 @@ class GamePanel(RelativeLayout):
 
     def setup(self):
         sz = (Window.size[0],Window.size[1]*0.95)
-        szp = [sz[1]/20, 0.95*sz[0]/10]
+        szp = [sz[1]/30, 0.95*sz[0]/10]
         
         self.size_hint = (1, 0.95)
         self.pos_hint = {'top':0.95}
@@ -36,11 +38,20 @@ class GamePanel(RelativeLayout):
         self.Ball.parent = self
         
         self.Canvas.setup(sz)
-        self.Ball.setup([sz[0]/2,sz[1]/2], 15)
+        self.Ball.setup([sz[0]/2,sz[1]/2], 5)
         self.PaddleLeft.setup([0,sz[1]/2], szp)
         self.PaddleRight.setup([sz[0]-szp[0],sz[1]/2], szp)
 
+        self.s1.pos_hint = { 'center_x':0.4 , 'center_y':0.5 }
+        self.s2.pos_hint = { 'center_x':0.6 , 'center_y':0.5 }
+
         Clock.schedule_interval(self.Ball.move, 1/30)
+
+    def score(self , team):
+        if team==0:
+            self.s1.text = str(int(self.s1.text)+1)
+        if team==1:
+            self.s2.text = str(int(self.s2.text)+1)
 
     def _on_key_down(self, keyboard, keycode, text, modifiers):
         if keycode[1]=='up':
--- Mechanics.py
+++ Mechanics.py
@@ -19,7 +19,6 @@ class Paddle():
         elif(self.position[1]+self.size[1]+self.velocity>self.parent.size[1] and direction==1):
             return
         self.position[1] += direction*self.velocity
-        self.parent.b1.text = str(self.position)
         self.on_position()
 
     def on_position(self):
@@ -36,10 +35,28 @@ class Ball():
         self.position = position
         self.radius = radius
         self.on_position()
+        self.p1 = self.parent.PaddleLeft 
+        self.p2 = self.parent.PaddleRight
 
     def move(self, time):
-        #vertical movement
-        if((self.position[1]-self.radius==0 and self.velocity[1]<0) or (self.position[1]+self.radius==self.parent.size[1] and self.velocity[1]>0)):
+
+        #horizontal movement
+        if((self.position[0]-self.radius==0) or (self.position[0]+self.radius==self.parent.size[0])):
+            self.velocity[0] *= -1
+            self.position[0] = self.velocity[0]+self.position[0]
+            if self.velocity[0]>0 :
+                self.parent.score(1)
+            else :
+                self.parent.score(0)
+        elif(self.position[0]-self.radius+self.velocity[0]<0):
+            self.position[0] = self.radius
+        elif(self.position[0]+self.velocity[0]+self.radius>self.parent.size[0]):
+            self.position[0] = self.parent.size[0]-self.radius
+        else:
+            self.position[0] = self.velocity[0]+self.position[0]
+
+        #general vertical movement
+        if((self.position[1]-self.radius==0) or (self.position[1]+self.radius==self.parent.size[1])):
             self.velocity[1] *= -1
             self.position[1] = self.velocity[1]+self.position[1]
         elif(self.position[1]-self.radius+self.velocity[1]<0):
@@ -49,18 +66,6 @@ class Ball():
         else:
             self.position[1] = self.velocity[1]+self.position[1]
 
-        #horizontal movement
-        if((self.position[0]-self.radius==0 and self.velocity[0]<0) or (self.position[0]+self.radius==self.parent.size[0] and self.velocity[0]>0)):
-            self.velocity[0] *= -1
-            self.position[0] = self.velocity[0]+self.position[0]
-        elif(self.position[0]-self.radius+self.velocity[0]<0):
-            self.position[0] = self.radius
-        elif(self.position[0]+self.velocity[0]+self.radius>self.parent.size[0]):
-            self.position[0] = self.parent.size[0]-self.radius
-        else:
-            self.position[0] = self.velocity[0]+self.position[0]
-
-        self.parent.b1.text = str(self.position) + str(self.velocity)
         self.on_position()
 
     def on_position(self):
