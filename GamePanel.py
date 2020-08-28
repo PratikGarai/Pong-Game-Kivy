034446a112b20e378fa94360ffbe2c27895a0eec
PratikGarai <pratikgarai0208@gmail.com>
2020-08-28 14:16:48 +0530

    Some changes

--- GamePanel.py
+++ GamePanel.py
@@ -2,6 +2,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.core.window import Window
+from Mechanics import Paddle, Ball
 
 class GamePanel(BoxLayout):
     def __init__(self, **kwargs):
@@ -11,6 +12,10 @@ class GamePanel(BoxLayout):
         self.b2 = Button(text="Right")
         self.add_widget(self.b1)
         self.add_widget(self.b2)
+
+        self.PaddleRight = Paddle(5)
+        self.PaddleLeft = Paddle(5)
+        self.Ball = Ball(5)
 
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_key_down)
@@ -23,6 +28,12 @@ class GamePanel(BoxLayout):
     def setup(self):
         self.size_hint = (1, 0.95)
         self.pos_hint = {'top':0.95}
+        self.PaddleRight.parent = self
+        self.PaddleLeft.parent = self
+        self.Ball.parent = self
+        self.Ball.setup([0.5,0.5])
+        self.PaddleLeft.setup([0,0.5])
+        self.PaddleRight.setup([1,0.5])
 
     def _on_key_down(self, keyboard, keycode, text, modifiers):
         if keycode[1]=='w':
--- Mechanics.py
+++ Mechanics.py
@@ -3,23 +3,27 @@ from kivy.properties import ListProperty
 class Paddle():
     position = ListProperty([0,0])
 
-    def __init__(self, position):
+    def __init__(self, velocity):
+        self.velocity = velocity
+
+    def setup(self, position):
         self.position = position
-        self.velocity = 5
 
     def move(self, direction):
         self.position[1] += direction*velocity
 
     def on_position(self, instance, pos):
-        #redraw on canvas
+        #nthing yet
         pass
 
 class Ball():
     position = ListProperty([0,0])
 
-    def __init__(self, position, velocity):
-        self.position = position
+    def __init__(self, velocity):
         self.velocity = velocity
+
+    def setup(self, position):
+        self.position = position
 
     def move(self, direction):
         self.position = [self.velocity[0]+self.position[0], self.velocity[1]+self.position[1]]
@@ -27,3 +31,5 @@ class Ball():
     def on_position(self, instance, pos):
         #redraw ball here
         pass
+
+class Canvas
